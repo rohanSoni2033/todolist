d8c05816dc2079017ba435fdbdc152269f49651c
rohan soni <rohansoni54476@gmail.com>
2022-11-03 10:07:30 +0530

    refactor App and index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import Create from './todoList/Create';
+import InCompleted from './todoList/InCompleted';
+import Completed from './todoList/Completed';
+
+const initialTodoList = [];
 
 function App() {
+  useEffect(() => {
+    const todoListItems = localStorage.getItem('react-todolist');
+    console.log(JSON.parse(todoListItems));
+  }, []);
+
+  const [completedTodoList, setCompletedTodoList] = useState(
+    initialTodoList.filter(todo => todo.complete)
+  );
+
+  const [inCompletedTodoList, setInCompletedTodoList] = useState(
+    initialTodoList.filter(todo => !todo.complete)
+  );
+
+  const createNewTodo = event => {
+    event.preventDefault();
+    const randomId = Math.floor(Math.random() * 1000000);
+    const id = randomId;
+    const title = event.target.todoName.value;
+    const todoItem = {
+      id,
+      title,
+      complete: false,
+      createdAt: new Date(),
+    };
+    setInCompletedTodoList([...inCompletedTodoList, todoItem]);
+
+    localStorage.setItem('react-todolist', JSON.stringify(todoItem));
+  };
+
+  const deleteCompletedTodo = id => {
+    const filteredArray = completedTodoList.filter(todo => todo.id !== id);
+
+    setCompletedTodoList(filteredArray);
+  };
+
+  const deleteInCompletedTodo = id => {
+    const filteredArray = inCompletedTodoList.filter(todo => todo.id !== id);
+
+    setInCompletedTodoList(filteredArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Create onCreateNewTodo={createNewTodo} />
+
+      <InCompleted
+        inCompleted={inCompletedTodoList}
+        completedListUpdate={setCompletedTodoList}
+        inCompletedListUpdate={setInCompletedTodoList}
+        completed={completedTodoList}
+        onDeleteTodo={deleteInCompletedTodo}
+      />
+
+      <Completed
+        completed={completedTodoList}
+        onDeleteTodo={deleteCompletedTodo}
+      />
+    </>
   );
 }
 
